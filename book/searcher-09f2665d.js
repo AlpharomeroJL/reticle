534837caa6024b0f302a104c25b27b55b610358c
Josef Long <Josefdean@protonmail.com>
2026-07-03 16:12:34 -0500

    Deploy Reticle v6.0.0 site: editor tools, agent surface, guided experience, book

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0df54dcb.js',
+                path_to_root + 'searchindex-cff64d34.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
