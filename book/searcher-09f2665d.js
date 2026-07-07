da787938f7505ef160dbd479b61261387d920b53
Josef Long <Josefdean@protonmail.com>
2026-07-07 18:14:24 -0500

    deploy: v8 Wave 3 (live DRC underlines, GPU heatmap, GPU-resident hierarchy, metrology; web-d9e257c768201db7)

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f14f2250.js',
+                path_to_root + 'searchindex-20d64bab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
