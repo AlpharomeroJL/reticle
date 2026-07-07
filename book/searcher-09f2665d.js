34ed7344398213edd904170dadd57329f15af46d
Josef Long <Josefdean@protonmail.com>
2026-07-07 01:14:55 -0500

    deploy: v7.0.0 site (web bundle web-8a430603a0e038f8, book)

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fd65c002.js',
+                path_to_root + 'searchindex-dd61d1be.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
